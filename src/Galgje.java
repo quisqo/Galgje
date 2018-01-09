54dbbee19dc649d93a961e5f50f6ed701db7b0a4
quisqo <quisqo@users.noreply.github.com>
2018-01-09 14:16:20 +0100

    meerdere letters invoeren

--- src/Galgje.java
+++ src/Galgje.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
 public class Galgje {
-	
-	public static void main (String[] args) {
-		
+
+	public static void main(String[] args) {
 		System.out.println("Welkom bij Galgje");
 		System.out.println("Het te raden woord");
-		
+
 		String teRadenWoord = "Qien";
-		
+
 		System.out.println(teRadenWoord);
 		Scanner sc = new Scanner(System.in);
-		String i = sc.nextLine();
-		System.out.println(i);
-		
-		if(teRadenWoord.contains(i)) {
-			System.out.println("ja");
-		}else {
-			System.out.println("nee");
-		}	
-		
-		System.out.println("Het spel is klaar");
-		
+		for (int j = 0; j < 10; j++) {
+			String i = sc.nextLine();
+			System.out.println("Uw ingevoerde letter is: " + i);
+			if (teRadenWoord.contains(i)) {
+				System.out.println("ja komt voor");
+			} else {
+				System.out.println("nee komt niet voor");
+			}
+
+			System.out.println("Het spel is klaar");
+
+		}
 	}
 
 }
