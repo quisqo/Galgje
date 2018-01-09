d6fa7592b8852a8782ca8234a0bccf6aa80392cc
quisqo <quisqo@users.noreply.github.com>
2018-01-09 14:38:23 +0100

    Instance variable gemaakt

--- src/Galgje.java
+++ src/Galgje.java
@@ -1,27 +1,30 @@
 import java.util.Scanner;
 
 public class Galgje {
-
+	public static String teRadenWoord = "Water"; //field instance variable
 	public static void main(String[] args) {
 		System.out.println("Welkom bij Galgje");
 		System.out.println("Het te raden woord");
 
-		String teRadenWoord = "Qien";
+		teRadenWoord = "Qien";
 
 		System.out.println(teRadenWoord);
 		Scanner sc = new Scanner(System.in);
 		for (int j = 0; j < 10; j++) {
 			String i = sc.nextLine();
-			System.out.println("Uw ingevoerde letter is: " + i);
-			if (teRadenWoord.contains(i)) {
-				System.out.println("ja komt voor");
-			} else {
-				System.out.println("nee komt niet voor");
-			}
-
-			System.out.println("Het spel is klaar");
-
+			checkenOfLetterBestaat( i);
 		}
+
+		System.out.println("Het spel is klaar");
+
 	}
 
+	public static void checkenOfLetterBestaat( String i2) {
+		System.out.println("Uw ingevoerde letter is: " + i2);
+		if (teRadenWoord.contains(i2)) {
+			System.out.println("ja, komt voor");
+		} else {
+			System.out.println("nee, komt niet voor");
+		}
+	}
 }
