c6386fb3ef103702a4230a54d4309484a69969ec
quisqo <quisqo@users.noreply.github.com>
2018-01-09 14:03:57 +0100

    letter invoer

--- src/Galgje.java
+++ src/Galgje.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Galgje {
 	
@@ -9,6 +10,17 @@ public class Galgje {
 		String teRadenWoord = "Qien";
 		
 		System.out.println(teRadenWoord);
+		Scanner sc = new Scanner(System.in);
+		String i = sc.nextLine();
+		System.out.println(i);
+		
+		if(teRadenWoord.contains(i)) {
+			System.out.println("ja");
+		}else {
+			System.out.println("nee");
+		}	
+		
+		System.out.println("Het spel is klaar");
 		
 	}
 
